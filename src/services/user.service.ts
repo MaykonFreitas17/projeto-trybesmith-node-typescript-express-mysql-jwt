73aae8925d3f5e1bf5e6752ff500e18ba272100c
Maykon Freitas <maykon17programmer@gmail.com>
2022-09-13 18:31:01 -0300

    feat: adiciona o metodo create no arquivo user.service.ts para criação de users

---
+++ src/services/user.service.ts
@@ -0,0 +1,30 @@
+import { sign, SignOptions } from 'jsonwebtoken';
+import connection from '../models/connection';
+import UserModel from '../models/user.model';
+import User from '../interfaces/user.interface';
+
+const secret = 'JWT_SECRET';
+
+class UserService {
+  public model: UserModel;
+
+  public jwtConfig: SignOptions;
+
+  constructor() {
+    this.model = new UserModel(connection);
+    this.jwtConfig = {
+      expiresIn: '8h',
+      algorithm: 'HS256',
+    };
+  }
+
+  public async create(user: User): Promise<object> {
+    const userCreated = await this.model.create(user);
+    const { username, password } = userCreated;
+    const payload = { username, password };
+    const token = sign(payload, secret, this.jwtConfig);
+    return { token };
+  }
+}
+
+export default UserService;
