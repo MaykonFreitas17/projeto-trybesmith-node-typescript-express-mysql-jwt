621111674a132494db5e302ca5aa216192b30313
Maykon Freitas <maykon17programmer@gmail.com>
2022-09-14 15:31:30 -0300

    feat: adiciona o arquivo user.routes.ts para configurar as rotas relacionadas ao login dos users | adiciona as rotas relacionas ao login dos users no arquivo app.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import validationUser from '../middlewares/user.middleware';
 
 const router = Router();
 
 const userController = new UserController();
 
 router.post('/users', userController.create);
+router.post('/login/', validationUser, userController.login);
 
 export default router;
