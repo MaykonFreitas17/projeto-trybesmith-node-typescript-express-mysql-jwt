632d152497520749921c2728f5b7e1fb858fe10f
Maykon Freitas <maykon17programmer@gmail.com>
2022-09-14 14:35:23 -0300

    feat: adicona o metedo login no arquivo user.model.ts para validar o login do usuario

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,6 +8,16 @@ class UserModel {
     this.connection = connection;
   }
 
+  public async login(username: string, password: string): Promise<User> {
+    const result = await this.connection.execute(
+      'SELECT * FROM users WHERE username=? AND password=?',
+      [username, password],
+    );
+    const [rows] = result;
+    const [user] = rows as User[];
+    return user;
+  }
+
   public async create(user: User): Promise<User> {
     const { username, classe, level, password } = user;
     const result = await this.connection.execute<ResultSetHeader>(
