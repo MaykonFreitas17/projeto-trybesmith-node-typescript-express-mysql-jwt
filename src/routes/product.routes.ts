28316bf094797054a3877746a75af10938bcc0d7
Maykon Freitas <maykon17programmer@gmail.com>
2022-09-14 17:43:03 -0300

    fix: modifica o arquivo product.routes.ts

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -7,7 +7,6 @@ const router = Router();
 const productController = new ProductController();
 
 router.get('/products/', productController.getAll);
-
 router.post('/products/', validationProduct, productController.create);
 
 export default router;
