5cc02751af4da0c48a24ee810c8327657d6f6495
Maykon Freitas <maykon17programmer@gmail.com>
2022-09-14 15:30:22 -0300

    fix: modifica a função de validação do login do user

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -2,18 +2,20 @@ import { NextFunction, Request, Response } from 'express';
 import connection from '../models/connection';
 import UserModel from '../models/user.model';
 
-function validationUser(req: Request, res: Response, next: NextFunction) {
+const validationUser = async (req: Request, res: Response, next: NextFunction) => {
   const model = new UserModel(connection);
   const { username, password } = req.body;
 
-  if (username === undefined) return res.status(400).json('"username" is required');
-  if (password === undefined) return res.status(400).json('"password" is required');
+  if (username === undefined) return res.status(400).json({ message: '"username" is required' });
+  if (password === undefined) return res.status(400).json({ message: '"password" is required' });
 
-  const login = model.login(username, password);
+  const login = await model.login(username, password);
 
-  if (!login) return res.status(401).json('Username or password invalid');
+  console.log(login);
+  
+  if (!login) return res.status(401).json({ message: 'Username or password invalid' });
 
   next();
-}
+};
 
 export default validationUser;
