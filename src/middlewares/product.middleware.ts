fb6d2c14cb14fc7bb9c8e2e380374a47e9a78539
Maykon Freitas <maykon17programmer@gmail.com>
2022-09-14 17:03:12 -0300

    feat/fix: adiciona função validationProduct | refatora funções validateName e validateAmount

--- src/middlewares/product.middleware.ts
+++ src/middlewares/product.middleware.ts
@@ -9,18 +9,34 @@ const errors = [
   '"amount" length must be at least 3 characters long',
 ];
 
-export const validationName = (req: Request, res: Response, next: NextFunction) => {
-  const { name } = req.body;
-  if (name === undefined) return res.status(400).json({ message: errors[0] });
-  if (typeof name !== 'string') return res.status(422).json({ message: errors[1] });
-  if (name.length <= 2) return res.status(422).json({ message: errors[2] });
+const validationName = (name: string) => {
+  if (name === undefined) return { message: errors[0], code: 400 };
+  if (typeof name !== 'string') return { message: errors[1], code: 422 };
+  if (name.length <= 2) return { message: errors[2], code: 422 };
+};
+
+const validationAmount = (amount: string) => {
+  if (amount === undefined) return { message: errors[3], code: 400 };
+  if (typeof amount !== 'string') return { message: errors[4], code: 422 };
+  if (amount.length <= 2) return { message: errors[5], code: 422 };
+};
+
+const validationProduct = (req: Request, res: Response, next: NextFunction) => {
+  const { name, amount } = req.body;
+  const validateName = validationName(name);
+  const validateAmount = validationAmount(amount);
+
+  if (typeof validateName === 'object') {
+    const { message, code } = validateName;
+    return res.status(code).json({ message });
+  }
+
+  if (typeof validateAmount === 'object') {
+    const { message, code } = validateAmount;
+    return res.status(code).json({ message });
+  }
+
   next();
 };
 
-export const validationAmount = (req: Request, res: Response, next: NextFunction) => {
-  const { amount } = req.body;
-  if (amount === undefined) return res.status(400).json({ message: errors[3] });
-  if (typeof amount !== 'string') return res.status(422).json({ message: errors[4] });
-  if (amount.length <= 2) return res.status(422).json({ message: errors[5] });
-  next();
-};
+export default validationProduct;
