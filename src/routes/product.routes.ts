8350050144c834db21b25407ee18715ae2e93fd2
Maykon Freitas <maykon17programmer@gmail.com>
2022-09-14 16:06:30 -0300

    feat: adiciona os middlewares na rota de cadastro de produtos no arquivo product.routes.ts

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import { validationName, validationAmount } from '../middlewares/product.middleware';
 
 const router = Router();
 
 const productController = new ProductController();
 
 router.get('/products/', productController.getAll);
+
+router.use(validationName);
+router.use(validationAmount);
 router.post('/products/', productController.create);
 
 export default router;
