06145e85639a7f1cc4b3c4edd3fa0b95366bf550
Maykon Freitas <maykon17programmer@gmail.com>
2022-09-14 17:10:03 -0300

    feat: adiciona os middlewares para validar na hora da criação de um novo users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
-import validationUser from '../middlewares/user.middleware';
+import validationUser,
+{ validationUsername,
+  validationClasse,
+  validationLevel,
+  validationPassword,
+} from '../middlewares/user.middleware';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/users', userController.create);
 router.post('/login/', validationUser, userController.login);
+
+router.use(validationUsername);
+router.use(validationClasse);
+router.use(validationLevel);
+router.use(validationPassword);
+router.post('/users', userController.create);
 
 export default router;
