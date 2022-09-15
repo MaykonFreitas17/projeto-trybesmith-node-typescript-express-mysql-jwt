2e5d77f89c5371c44a6e1e61d8196a8fa960270b
Maykon Freitas <maykon17programmer@gmail.com>
2022-09-14 21:09:04 -0300

    feat: adiciona a rota de cadastro de novo orders no arquivo order.routes.ts | adicona o middleware de valicação do token na rota de cadastro de orders

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import OrderController from '../controllers/order.controller';
+import validationToken from '../middlewares/token.middleware';
 
 const router = Router();
 
 const orderController = new OrderController();
-
 router.get('/orders/', orderController.getAll);
+router.post('/orders/', validationToken, orderController.create);
 
 export default router;
